13925a807d536cb38b1d32c0a0b534ed99afa77e
msalihcavus <m.salihcavus@hotmail.com>
2022-06-23 12:57:00 +0400

    comm key vault

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const {ClientSecretCredential} = require('@azure/identity')
 const {SecretClient} = require('@azure/keyvault-secrets')
 
 const credential = new ClientSecretCredential(
-    '90386a23-5656-47c3-bd6f-ca12bf2b8ed4', 
-    'f3ce8171-0db4-447a-89e1-613b5773a535', 
-    'p1j-PDg3c5~S1EDTivXl0.K.grXW20~65.'
+    '27287a8c-b44a-40a0-a8cd-a5be11977eef', //tenant id
+    'd87aead6-eb88-4fbc-9bd3-6bbac3774094',  //client id
+    'VdT8Q~MMmDz5e8i6FIO5bLYQVRiDXH~xX8zZcc.i'   //client secret
     )
 
-const client = new SecretClient('https://pixmyvault.vault.azure.net/', credential)
+const client = new SecretClient('https://salovault204.vault.azure.net/', credential)
 
-client.getSecret('mysecret')
+client.getSecret('mysecret2')
 .then((response) => {
-    console.log('secret value: ', response.value)
+    console.log(response)
 })
 .catch((err) => {
     console.log('error', err)
